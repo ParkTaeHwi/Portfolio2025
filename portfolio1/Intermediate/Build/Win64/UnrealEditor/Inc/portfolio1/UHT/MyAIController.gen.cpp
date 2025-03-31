6cf8ee20d00e07c60f1629e1d2a45aaf11406b25
ParkTaeHwi <113888348+ParkTaeHwi@users.noreply.github.com>
2025-03-31 12:50:37 +0900

    MyAiController

---
+++ portfolio1/Intermediate/Build/Win64/UnrealEditor/Inc/portfolio1/UHT/MyAIController.gen.cpp
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "portfolio1/MyAIController.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeMyAIController() {}
+
+// Begin Cross Module References
+AIMODULE_API UClass* Z_Construct_UClass_AAIController();
+PORTFOLIO1_API UClass* Z_Construct_UClass_AMyAIController();
+PORTFOLIO1_API UClass* Z_Construct_UClass_AMyAIController_NoRegister();
+UPackage* Z_Construct_UPackage__Script_portfolio1();
+// End Cross Module References
+
+// Begin Class AMyAIController
+void AMyAIController::StaticRegisterNativesAMyAIController()
+{
+}
+IMPLEMENT_CLASS_NO_AUTO_REGISTRATION(AMyAIController);
+UClass* Z_Construct_UClass_AMyAIController_NoRegister()
+{
+	return AMyAIController::StaticClass();
+}
+struct Z_Construct_UClass_AMyAIController_Statics
+{
+#if WITH_METADATA
+	static constexpr UECodeGen_Private::FMetaDataPairParam Class_MetaDataParams[] = {
+#if !UE_BUILD_SHIPPING
+		{ "Comment", "/**\n * \n */" },
+#endif
+		{ "HideCategories", "Collision Rendering Transformation" },
+		{ "IncludePath", "MyAIController.h" },
+		{ "ModuleRelativePath", "MyAIController.h" },
+	};
+#endif // WITH_METADATA
+	static UObject* (*const DependentSingletons[])();
+	static constexpr FCppClassTypeInfoStatic StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<AMyAIController>::IsAbstract,
+	};
+	static const UECodeGen_Private::FClassParams ClassParams;
+};
+UObject* (*const Z_Construct_UClass_AMyAIController_Statics::DependentSingletons[])() = {
+	(UObject* (*)())Z_Construct_UClass_AAIController,
+	(UObject* (*)())Z_Construct_UPackage__Script_portfolio1,
+};
+static_assert(UE_ARRAY_COUNT(Z_Construct_UClass_AMyAIController_Statics::DependentSingletons) < 16);
+const UECodeGen_Private::FClassParams Z_Construct_UClass_AMyAIController_Statics::ClassParams = {
+	&AMyAIController::StaticClass,
+	"Engine",
+	&StaticCppClassTypeInfo,
+	DependentSingletons,
+	nullptr,
+	nullptr,
+	nullptr,
+	UE_ARRAY_COUNT(DependentSingletons),
+	0,
+	0,
+	0,
+	0x009002A4u,
+	METADATA_PARAMS(UE_ARRAY_COUNT(Z_Construct_UClass_AMyAIController_Statics::Class_MetaDataParams), Z_Construct_UClass_AMyAIController_Statics::Class_MetaDataParams)
+};
+UClass* Z_Construct_UClass_AMyAIController()
+{
+	if (!Z_Registration_Info_UClass_AMyAIController.OuterSingleton)
+	{
+		UECodeGen_Private::ConstructUClass(Z_Registration_Info_UClass_AMyAIController.OuterSingleton, Z_Construct_UClass_AMyAIController_Statics::ClassParams);
+	}
+	return Z_Registration_Info_UClass_AMyAIController.OuterSingleton;
+}
+template<> PORTFOLIO1_API UClass* StaticClass<AMyAIController>()
+{
+	return AMyAIController::StaticClass();
+}
+AMyAIController::AMyAIController(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {}
+DEFINE_VTABLE_PTR_HELPER_CTOR(AMyAIController);
+AMyAIController::~AMyAIController() {}
+// End Class AMyAIController
+
+// Begin Registration
+struct Z_CompiledInDeferFile_FID_AUnreal5_Portfolio2025_portfolio1_Source_portfolio1_MyAIController_h_Statics
+{
+	static constexpr FClassRegisterCompiledInInfo ClassInfo[] = {
+		{ Z_Construct_UClass_AMyAIController, AMyAIController::StaticClass, TEXT("AMyAIController"), &Z_Registration_Info_UClass_AMyAIController, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(AMyAIController), 670896040U) },
+	};
+};
+static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_AUnreal5_Portfolio2025_portfolio1_Source_portfolio1_MyAIController_h_3249431269(TEXT("/Script/portfolio1"),
+	Z_CompiledInDeferFile_FID_AUnreal5_Portfolio2025_portfolio1_Source_portfolio1_MyAIController_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_AUnreal5_Portfolio2025_portfolio1_Source_portfolio1_MyAIController_h_Statics::ClassInfo),
+	nullptr, 0,
+	nullptr, 0);
+// End Registration
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
--- portfolio1/Intermediate/Build/Win64/UnrealEditor/Inc/portfolio1/UHT/portfolio1.init.gen.cpp
+++ portfolio1/Intermediate/Build/Win64/UnrealEditor/Inc/portfolio1/UHT/portfolio1.init.gen.cpp
@@ -21,13 +21,13 @@ void EmptyLinkFunctionForGeneratedCodeportfolio1_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x9946DB0F,
-				0x64DF9137,
+				0x686AF303,
+				0x5A456CE1,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_portfolio1.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_portfolio1.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_portfolio1(Z_Construct_UPackage__Script_portfolio1, TEXT("/Script/portfolio1"), Z_Registration_Info_UPackage__Script_portfolio1, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x9946DB0F, 0x64DF9137));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_portfolio1(Z_Construct_UPackage__Script_portfolio1, TEXT("/Script/portfolio1"), Z_Registration_Info_UPackage__Script_portfolio1, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x686AF303, 0x5A456CE1));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
