243b53fdaf3840c21d228847c4fd1ed164e6c42b
ParkTaeHwi <113888348+ParkTaeHwi@users.noreply.github.com>
2025-04-28 18:28:51 +0900

    portfolio2

---
+++ portfolio2/Source/portfolio2/portfolio2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "portfolio2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, portfolio2, "portfolio2" );
