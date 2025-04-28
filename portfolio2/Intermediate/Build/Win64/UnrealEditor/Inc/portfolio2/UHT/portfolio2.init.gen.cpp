fdac987da36378e25ca2350b246fcb00f8ee7704
ParkTaeHwi <113888348+ParkTaeHwi@users.noreply.github.com>
2025-04-28 18:48:58 +0900

    1

---
+++ portfolio2/Intermediate/Build/Win64/UnrealEditor/Inc/portfolio2/UHT/portfolio2.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeportfolio2_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_portfolio2;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_portfolio2()
+	{
+		if (!Z_Registration_Info_UPackage__Script_portfolio2.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/portfolio2",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x0BFBBF72,
+				0x14CE1EBC,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_portfolio2.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_portfolio2.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_portfolio2(Z_Construct_UPackage__Script_portfolio2, TEXT("/Script/portfolio2"), Z_Registration_Info_UPackage__Script_portfolio2, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x0BFBBF72, 0x14CE1EBC));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
